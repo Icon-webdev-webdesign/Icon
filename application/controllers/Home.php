92f1cb28525836d232c6ad40b159fc8bb93385c2
ravitejakambaluru <68679704+ravitejakambaluru@users.noreply.github.com>
2020-07-23 12:23:31 +0530

    Add files via upload

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php 
+
+	/**
+	 * 
+	 */
+	class Home extends CI_Controller
+	{
+		
+		public function index()
+		{
+			$this->load->view('template/header');
+			$this->load->view('template/footer');
+		}
+	}
+
+?>
